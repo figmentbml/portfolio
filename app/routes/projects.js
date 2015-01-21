d32a45fe43bd4828ab2204abebb779c73e0d682d
figmentbml <beth.jaswa@gmail.com>
2015-01-21 15:05:34 -0700

    new style

---
+++ app/routes/projects.js
@@ -0,0 +1,35 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model: function () {
+    return [
+      {
+        id: "1",
+        name: "gCamp",
+        description: "A Rails app that has some of the same functionality as Basecamp.",
+        url: "http://stormy-cove-1200.herokuapp.com/",
+        github: "https://github.com/figmentbml/gCamp",
+        skills: ["Ruby", "Rails", "Rspec", "Capybara"],
+      },
+      {
+        id: "2",
+        name: "Colorado Wine Tracker",
+        description: "Personal Project",
+        url: "http://www.wine-tracker.co/",
+        github: "https://github.com/figmentbml/wine-tracker",
+        skills: ["Rake Tasks", "Scraping Sites", "Yelp API", "Carrierwave & Fog"],
+      },
+      {
+        id: "3",
+        name: "Listen",
+        description: "Simple Music Player",
+        url: "http://development.listen-beth-j.divshot.io/",
+        github: "https://github.com/figmentbml/listen",
+        skills: ["JavaScript click events", "jQuery", "HTML audio tags"],
+      },
+
+      ];
+  }
+
+});
---
+++ app/routes/resume.js
@@ -0,0 +1,22 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  model: function () {
+    return [
+    {
+      employer: "Cupcake",
+      title: "Cupcake ipsum dolor sit amet biscuit carrot cake bear claw ice cream. Oat cake ice cream marshmallow jelly beans donut fruitcake cake sweet. Powder marzipan biscuit pudding. Topping biscuit fruitcake. Croissant chocolate tart jelly ice cream dragée. Caramels pie macaroon jelly-o wafer.",
+      dates: "www.google.com",
+      description: "www.github",
+    },
+    {
+      employer: "Cupcake",
+      title: "Cupcake ipsum dolor sit amet biscuit carrot cake bear claw ice cream. Oat cake ice cream marshmallow jelly beans donut fruitcake cake sweet. Powder marzipan biscuit pudding. Topping biscuit fruitcake. Croissant chocolate tart jelly ice cream dragée. Caramels pie macaroon jelly-o wafer.",
+      dates: "www.google.com",
+      description: "www.github",
+    },
+    ];
+  }
+
+});
