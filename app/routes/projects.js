728c375d86f789e2fb9e165c898d5ed9aad911a7
figmentbml <beth.jaswa@gmail.com>
2015-02-02 11:42:57 -0700

    added pics to projects

--- app/routes/projects.js
+++ app/routes/projects.js
@@ -6,19 +6,21 @@ export default Ember.Route.extend({
     return [
       {
         id: "1",
+        name: "Colorado Wine Tracker",
+        description: "This app provides information Colorado Wineries and their wine. It also keeps track of your tasting notes so you can remember if you liked that wine or not.",
+        url: "http://www.wine-tracker.co/",
+        github: "https://github.com/figmentbml/wine-tracker",
+        skills: ["Ruby on Rails", "Gems: Yelp API, Carrierwave, Fog, Nokogiri", "Rake Tasks", "Scraping Sites", "Forms", "User Authorization", "Bootstrap", "Associations", "Controller Specs"],
+        image: "/assets/Screen Shot wine tracker.png",
+      },
+      {
+        id: "2",
         name: "gCamp",
         description: "A Rails app that has some of the same functionality as Basecamp.",
         url: "http://stormy-cove-1200.herokuapp.com/",
         github: "https://github.com/figmentbml/gCamp",
-        skills: ["Ruby", "Rails", "Join Tables", "User Authorization", "Rspec", "Capybara"],
-      },
-      {
-        id: "2",
-        name: "Colorado Wine Tracker",
-        description: "Personal Project",
-        url: "http://www.wine-tracker.co/",
-        github: "https://github.com/figmentbml/wine-tracker",
-        skills: ["Rake Tasks", "Scraping Sites", "Yelp API", "Carrierwave & Fog", "Forms"],
+        skills: ["Ruby on Rails", "Join Tables", "User Authorization", "Rspec", "Capybara"],
+        image: "/assets/Screen Shot gcamp.png",
       },
       {
         id: "3",
@@ -27,6 +29,7 @@ export default Ember.Route.extend({
         url: "http://development.listen-beth-j.divshot.io/",
         github: "https://github.com/figmentbml/listen",
         skills: ["JavaScript click events", "jQuery", "HTML audio tags"],
+        image: "",
       },
 
       ];
--- app/routes/resume.js
+++ app/routes/resume.js
@@ -10,15 +10,15 @@ export default Ember.Route.extend({
       dates: "Feb 2012 to Sep 2014",
       location: "Boulder, CO",
       description: [
-      "Ensure client's brand standards are consistently achieved and engage onsite customers on a daily basis",
-      "Train Cafe Managers for other sites on internal tools, events, and client culture",
-      "Manage 11 MicroKitchens in 4 buildings to insure that they offer variety of products and are stocked with items ordered from multiple vendors at $6,000 per week",
-      "Manage internal Google +page with promotions, events, and menu pictures",
-      "Distribute menu for both cafes detailing ingredients and allergens",
-      "Manage all feedback, compliments, and requests from customers and corporate client",
-      "Input all bills and manage service contracts from multiple vendors",
-      "Plan and execute events ranging from MicroKitchen Fair, Annual Farm to Plate, weekly happy hours, and other catering opportunities as needed",
-      "Coordinate CSA sign ups and distribute shares for office CSA programs"
+      "Ensured client's brand standards are consistently achieved and engaged onsite customers on a daily basis",
+      "Trained Cafe Managers for other sites on internal tools, events, and client culture",
+      "Managed 11 MicroKitchens in 4 buildings to insure that they offer variety of products and are stocked with items ordered from multiple vendors at $6,000 per week",
+      "Managed internal Google +page with promotions, events, and menu pictures",
+      "Met very specific and complicated client needs around food safety and allergens",
+      "Managed all feedback, compliments, and requests from customers and corporate client",
+      "Inputed all bills and manage service contracts from multiple vendors",
+      "Planned and executed events ranging from MicroKitchen Fair, Annual Farm to Plate, weekly happy hours, and other catering opportunities as needed",
+      "Coordinated CSA sign ups and distribute shares for office CSA programs"
       ],
     },
     {
@@ -26,15 +26,36 @@ export default Ember.Route.extend({
       title: "Catering Production and Server",
       dates: "Apr 2010 to Feb 2012",
       location: "Boulder, CO",
-      description: [],
+      description: [
+      "Set up breakfast and lunch buffet for 150 with a rotating menu",
+      "Ordered supplies from main kitchen and vendors on a daily and weekly basis",
+      "Worked independently with little to no interaction from manager",
+      "Stocked Micro Kitchens with assorted beverages, fruit, and snacks"
+      ],
     },
     {
       employer: "Loews Hotels",
-      title: "Banquet Captain and Restaurant Supervisor",
+      title: "Banquet Captain and Restaurant Manager",
       dates: "Sep 2009 to Apr 2010",
       location: "Denver, CO",
-      description: [],
+      description: [
+      "Managed banquets and meetings of up to 100 as well as multiple functions at one time",
+      "Worked closely with the Sales team and the client to ensure that everything flowed smoothly",
+      "Trained staff on Loews standards and acted as Manager On Duty for entire hotel on Sunday nights",
+      "Supervised restaurant to ensure proper ticket times and maintain customer satisfaction",
+      "Reorganized the wine menu to promote server knowledge and increase wine sales"
+      ],
+    },
+    {
+      employer: "Hilton Norfolk Airport",
+      title: "Banquet Captain",
+      dates: "May 2008 to Apr 2009",
+      location: "Norfolk, VA",
+      description: [
+      "Managed banquets and meetings of up to 300 attendees in different rooms"
+      ],
     }
+
     ];
   }
 
